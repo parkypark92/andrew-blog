1d8f2bf6eb5e170e3b1e2ec83d61f8f12ebc6fc2
Andrew <115462877+parkypark92@users.noreply.github.com>
2024-10-17 10:20:27 +0100

    implement basic API call to get all blogposts and display posts data on the homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,32 @@
+import { useState } from "react";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 export default function Home() {
+  const [postsData, setPostsData] = useState(null);
+
+  useEffect(() => {
+    async function getPostData() {
+      const data = await axios.get("http://localhost:3000/blogposts");
+      setPostsData(data.data.postsData);
+    }
+    getPostData();
+  }, []);
+
   return (
     <div>
+      <div className="posts-cntr">
+        {postsData &&
+          postsData.map((post) => {
+            return (
+              <div key={post.id} className="post">
+                <h2>{post.title}</h2>
+                <p>{post.content}</p>
+              </div>
+            );
+          })}
+      </div>
       <Link to="/create-post">
         <button>Create Post</button>
       </Link>
